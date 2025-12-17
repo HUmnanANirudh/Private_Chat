ebf5360cd73ed6c6898dfae7ce799d4a0fd3ad4b
HUmnanANirudh <tanirudhganesh@gmail.com>
2025-12-17 20:59:15 +0530

    refactor:enhanced username generation feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,86 @@
-import Image from "next/image";
+"use client"
+import { useEffect, useState } from "react";
+import { RefreshCcw } from 'lucide-react';
+const Names  = [
+  "Aether",
+  "Nyx",
+  "Orion",
+  "Loki",
+  "Zephyr",
+  "Atlas",
+  "Fenrir",
+  "Kairo",
+  "Raven",
+  "Onyx",
+  "Valkyrie",
+  "Draco",
+  "Sable",
+  "Echo",
+  "Cosmo",
+  "Artemis",
+  "Wolfgang",
+  "Storm",
+  "Leon",
+  "Nova",
+  "Hades",
+  "Kitsune",
+  "Shadow",
+  "Blaze",
+  "Falcon",
+  "Zorro",
+  "Titan"
+];
+
+const usernameGenrator =  () =>{
+  const name = Names[Math.floor(Math.random() * Names.length)];
+  const id = crypto.randomUUID().slice(0,6);
+  return `${name}_${id}`
+}
 
 export default function Home() {
+  const [userName,setuserName]  =  useState('');
+  useEffect(() => {
+    const stored = localStorage.getItem("username");
+    if(stored) {
+      setuserName(stored);
+    } else {
+      const newUserName = usernameGenrator();
+      localStorage.setItem("username", newUserName);
+      setuserName(newUserName);
+    }
+  }, []);
+const regenerate = ()=>{
+  localStorage.removeItem("username");
+  const username = usernameGenrator();
+  localStorage.setItem("username",username);
+  setuserName(username);
+}
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+  <main className="min-h-screen w-full flex items-center justify-center bg-zinc-950 px-4 pt-12">
+  <div className="w-full max-w-md -translate-y-16">
+     <div className="flex justify-center items-center mb-8">
+    <h1 className="text-3xl font-semibold text-zinc-100 my-4">{">_"} Private_Chat</h1>
     </div>
+    <div className="rounded-md border border-zinc-800 bg-zinc-900 p-6 shadow-2xl shadow-amber-50">
+      <h1 className="text-2xl font-semibold text-zinc-100 mb-1">
+        Your Identity
+      </h1>
+      <div className="flex items-center justify-between rounded-lg border border-zinc-700 bg-zinc-950 my-4 px-4 py-4">
+        <span className="text-sm font-medium text-zinc-100">
+          {userName}
+        </span>
+        <button onClick={regenerate} className="ml-4  py-1 bg-zinc-900 text-zinc-100 rounded-full hover:text-zinc-400 cursor-pointer">
+          <RefreshCcw />
+        </button>
+      </div>
+      <div className="flex justify-center items-center w-full">
+        <button className="p-4 bg-zinc-100 text-background font-bold hover:bg-zinc-200 hover:cursor-pointer hover:scale-105 transition-all ease-in-out duration-300 active:scale-75 text-sm">
+          CREATE NEW CHAT ROOM
+        </button>
+      </div>
+    </div>
+  </div>
+</main>
+
   );
 }
