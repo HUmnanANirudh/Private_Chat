adec06783201074f3ce717595f92646476923597
HUmnanANirudh <tanirudhganesh@gmail.com>
2026-05-18 18:38:14 +0530

    feat: implement joinRoomHandler for WebSocket room authentication and validation

---
+++ apps/ws/src/handlers/joinRoom.ts
@@ -0,0 +1,21 @@
+import type { ServerWebSocket } from "bun";
+import { registry } from "../registry";
+import type { wsData } from "../types";
+export const joinRoomHandler = async (ws: ServerWebSocket<wsData>,roomId:string,token:string) =>{
+    let room = registry.get(roomId);
+    const existingRoom = await getRoomMeta(String(roomId));
+    if (Object.keys(existingRoom.meta || {}).length === 0 ) {
+        ws.send(JSON.stringify({
+            type: "error",
+            message: "Room not found",
+        }));
+        ws.close();
+        return;
+    }
+    if (existingRoom.participants.includes(String(token))) {
+      ws.send(JSON.stringify({
+        type: "success",
+        message: "Welcome back to the room",
+        roomId,
+      }))
+    }}
